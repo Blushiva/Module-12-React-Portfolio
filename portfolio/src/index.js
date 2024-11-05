83b75e005d8f30e0b0e66799f7177b6d560ceab7
Brandi R <167109599+Blushiva@users.noreply.github.com>
2024-11-05 12:38:52 -0600

    added font styles

--- portfolio/src/index.js
+++ portfolio/src/index.js
@@ -4,6 +4,9 @@ import './index.css';
 import App from './App';
 import reportWebVitals from './reportWebVitals';
 
+import "@fontsource/roboto";
+import "@fontsource/outfit";
+
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
   <React.StrictMode>
