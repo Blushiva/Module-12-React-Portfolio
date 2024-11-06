d9b3b116a08fe54d2bbd8cbdd68796c094018d9f
Brandi R <167109599+Blushiva@users.noreply.github.com>
2024-11-06 16:03:41 -0600

    added files

--- portfolio/src/App.js
+++ portfolio/src/App.js
@@ -1,11 +1,13 @@
 
 import styles from './AppModule.css';
 
-function App[] {  
-  return div className=(styles.App) {
-    h1 {
-      'Hello, world!'
-    }
-  }
-
+function App() {
+  return (
+    <div className={styles.App}>
+      <h1>
+        Hello, world!
+      </h1>
+    </div>
+  );
+}
 export default App;
